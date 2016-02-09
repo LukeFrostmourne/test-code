ec7221d9fbccf25fc6722fd375b0f423899261c2
Li Feixiang <jp20838@w022501507019m.local>
2016-02-09 16:02:03 +0900

    add zip file

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello'
+    return 'hello world ! welcome to use marathon'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port=8000)
