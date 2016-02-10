fd512f20627f21d2cd9de1ab1250aad1a2f83b7b
liFeixiang <lifeixiangchina@gmail.com>
2016-02-10 17:21:43 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'hello world ! welcome to use marathon v2'
+    
+@app.route('/version')
+def print_version():
+    return 'python3.5 flask0.10'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000)
+    app.run(host="0.0.0.0")
