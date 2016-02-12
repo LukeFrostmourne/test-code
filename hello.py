da43b08db5b809d3afc2e58e692974e6f8b96075
liFeixiang <lifeixiangchina@gmail.com>
2016-02-12 10:44:20 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,5 +9,22 @@ def hello_world():
 def print_version():
     return 'python3.5 flask0.10'
 
+import optparse
+def flaskrun(app, default_host="127.0.0.1", default_port="5000"):
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname of the Flask app " + \
+                           "[default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the Flask app " + \
+                           "[default %s]" % default_port,
+                      default=default_port)
+    options, _ = parser.parse_args()
+    app.run(
+        host=options.host,
+        port=int(options.port)
+    )
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    flaskrun(app)
